2ec5332539a5a9ae85d3192a14f5410eda762784
yasha-dev1 <yasha1boroumand@gmail.com>
2024-05-23 13:58:56 +0200

    feat: removed setting edge connection to all strings

--- src/backend/base/langflow/template/field/base.py
+++ src/backend/base/langflow/template/field/base.py
@@ -82,7 +82,7 @@ class TemplateField(BaseModel):
     def serialize_model(self, handler):
         result = handler(self)
         # If the field is str, we add the Text input type
-        if self.field_type in ["str", "Text"]:
+        if self.field_type in ["Text"]:
             if "input_types" not in result:
                 result["input_types"] = ["Text"]
         if self.field_type == "Text":
